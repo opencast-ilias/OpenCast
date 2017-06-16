0d48e2fbe4962fd771dca1528b7cd60d343990a9
Stefan Wanzenried <sw@studer-raimann.ch>
2017-06-16 16:08:37 +0200

    [FIX] Invalid argument supplied for foreach()

--- classes/Series/class.xoctSeries.php
+++ classes/Series/class.xoctSeries.php
@@ -246,7 +246,7 @@ class xoctSeries extends xoctObject {
 			return $existing;
 		}
 		$return = array();
-		$data = json_decode(xoctRequest::root()->series()->get('', array( $user_string )));
+		$data = (array) json_decode(xoctRequest::root()->series()->get('', array( $user_string )));
 		foreach ($data as $d) {
 			$obj = new self();
 			try {
