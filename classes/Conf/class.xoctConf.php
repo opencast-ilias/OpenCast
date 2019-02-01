d2d7c652b9b1199bae98ceadfd09373c045d8543
Benjamin Hempel <Benjamin.Hempel@rfh-koeln.de>
2019-02-01 11:15:13 +0100

    fixed typo

--- classes/Conf/class.xoctConf.php
+++ classes/Conf/class.xoctConf.php
@@ -63,7 +63,7 @@ class xoctConf extends ActiveRecord {
 
 	const F_USE_STREAMING = 'use_streaming';
     const F_STREAMING_URL = 'streaming_url';
-    const F_USE_HIGHLOWRESSEGMENTPREVIEWS = 'use_highlowres_segemnt_preview';
+    const F_USE_HIGHLOWRESSEGMENTPREVIEWS = 'use_highlowres_segment_preview';
     const F_SEGMENT_TAG = 'segment_tag';
 
 	/**
