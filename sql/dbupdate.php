20b282d73627060c833fcd2e1055e97f9e4995a3
Theodor Truffer <tt@studer-raimann.ch>
2019-07-25 11:45:35 +0200

    remove Event database for development

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -158,5 +158,4 @@ xoctUserViewType::updateDB();
 \srag\Plugins\Opencast\Chat\ChatroomAR::updateDB();
 \srag\Plugins\Opencast\Chat\MessageAR::updateDB();
 \srag\Plugins\Opencast\Chat\TokenAR::updateDB();
-\srag\Plugins\Opencast\Chat\EventAR::updateDB();
 ?>
