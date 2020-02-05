bb3f97c6560b7aa0df12f54cc0f01e69720c10cb
Theodor Truffer <tt@studer-raimann.ch>
2020-02-05 17:19:13 +0100

    PLOPENCAST-196

--- classes/Event/class.xoctEventTableGUI.php
+++ classes/Event/class.xoctEventTableGUI.php
@@ -300,7 +300,7 @@ class xoctEventTableGUI extends ilTable2GUI {
 	 */
 	protected function parseData() {
 		$filter = array( 'series' => $this->xoctOpenCast->getSeriesIdentifier() );
-		$a_data = $this->event_repository->getFiltered($filter, '', [], $this->getOffset(), $this->getLimit());
+		$a_data = $this->event_repository->getFiltered($filter, '', []);
 
 		$a_data = array_filter($a_data, $this->filterPermissions());
 		$a_data = array_filter($a_data, $this->filterArray());
