cbcb039695ec69c87eaee55fe7d889ab01564548
Theodor Truffer <theo@fluxlabs.ch>
2021-12-03 12:09:30 +0100

    repeated scheduling working

--- src/Model/API/Scheduling/RRule.php
+++ src/Model/API/Scheduling/RRule.php
@@ -2,6 +2,8 @@
 
 namespace srag\Plugins\Opencast\Model\API\Scheduling;
 
+use DateTimeImmutable;
+
 class RRule
 {
     /**
@@ -17,12 +19,10 @@ class RRule
         $this->value = $value;
     }
 
-    public static function fromStartAndWeekdays(\DateTimeImmutable $start, array $weekdays) : self
+    public static function fromStartAndWeekdays(DateTimeImmutable $start, array $weekdays) : self
     {
-        $start_ts = $start->getTimestamp();
-        $byhour = floor($start_ts / 3600);
-        $byminute = floor($start_ts / 60) % 60;
-
+        $byhour = $start->format('H');
+        $byminute = $start->format('i');
         $byday = implode(',', $weekdays);
         $rrule = "FREQ=WEEKLY;BYDAY=$byday;BYHOUR=$byhour;BYMINUTE=$byminute;";
         return new self($rrule);
