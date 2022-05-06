87e0272ee4e04cbd38bd8ca1c11a59790c023687
Fabian Schmid <fabian@sr.solutions>
2022-05-06 14:08:14 +0200

    Checks and redme for renaming

--- classes/class.ilOpenCastPlugin.php
+++ classes/class.ilOpenCastPlugin.php
@@ -30,101 +30,142 @@ require_once __DIR__ . '/../vendor/autoload.php';
  * @version 1.0.00
  *
  */
-class ilOpenCastPlugin extends ilRepositoryObjectPlugin {
-
-	const PLUGIN_CLASS_NAME = self::class;
-
-	use DataTableUITrait;
-	use DICTrait;
-
-	const PLUGIN_ID = 'xoct';
-	const PLUGIN_NAME = 'OpenCast';
+class ilOpenCastPlugin extends ilRepositoryObjectPlugin
+{
+    
+    const PLUGIN_CLASS_NAME = self::class;
+    
+    use DataTableUITrait;
+    use DICTrait;
+    
+    const PLUGIN_ID = 'xoct';
+    const PLUGIN_NAME = 'OpenCast';
     /**
      * @var ilDBInterface
      */
-	protected $db;
-
-
-	/**
-	 *
-	 */
-	public function __construct() {
-		parent::__construct();
-
-		global $DIC;
-		$this->db = $DIC->database();
-	}
-
-	/**
-	 *
-	 */
-	protected function afterUpdate()
-	{
-		if (PluginConfig::count() == 0) {
-			PluginConfig::importFromXML($this->getDirectory() . '/configuration/default_config.xml');
-		}
-	}
-
-	/**
-	 * @return bool
-	 */
-	protected function uninstallCustom() {
-		$this->db->dropTable(PermissionGrant::TABLE_NAME, false);
-		$this->db->dropTable(PermissionGroupParticipant::TABLE_NAME, false);
-		$this->db->dropTable(PermissionGroup::TABLE_NAME, false);
-		$this->db->dropTable(ObjectSettings::TABLE_NAME, false);
-		$this->db->dropTable(EventAdditionsAR::TABLE_NAME, false);
-		$this->db->dropTable(PermissionTemplate::TABLE_NAME, false);
-		$this->db->dropTable(PublicationUsage::TABLE_NAME, false);
-		$this->db->dropTable(PluginConfig::TABLE_NAME, false);
-		$this->db->dropTable(Report::DB_TABLE, false);
-		$this->db->dropTable(WorkflowAR::TABLE_NAME, false);
-		$this->db->dropTable(WorkflowParameter::TABLE_NAME, false);
-		$this->db->dropTable(SeriesWorkflowParameter::TABLE_NAME, false);
-		$this->db->dropTable(MDFieldConfigEventAR::TABLE_NAME, false);
-		$this->db->dropTable(MDFieldConfigSeriesAR::TABLE_NAME, false);
-		$this->db->dropTable(UserSetting::TABLE_NAME, false);
-		$this->db->dropTable(AcceptedToU::TABLE_NAME, false);
-		$this->db->dropTable(DBCacheAR::TABLE_NAME, false);
-
-		return true;
-	}
-
-
-	/**
-	 * @var ilOpenCastPlugin
-	 */
-	protected static $cache;
-
-
-	/**
-	 * @return ilOpenCastPlugin
-	 */
-	public static function getInstance() {
-		if (!isset(self::$cache)) {
-			self::$cache = new self();
-		}
-
-		return self::$cache;
-	}
-
-
-	/**
-	 * @return string
-	 */
-	function getPluginName() {
-		return self::PLUGIN_NAME;
-	}
-
-	public function allowCopy()
-	{
-		return true;
-	}
-
-	public function updateLanguages($a_lang_keys = null)
-	{
-		parent::updateLanguages($a_lang_keys);
-		self::dataTableUI()->installLanguages(self::plugin());
-	}
-
+    protected $db;
+    
+    /**
+     * @var ilOpenCastPlugin
+     */
+    protected static $cache;
+    
+    public static function getInstance() : ilOpenCastPlugin
+    {
+        if (!isset(self::$cache)) {
+            self::$cache = new self();
+        }
+        
+        return self::$cache;
+    }
+    
+    /**
+     *
+     */
+    public function __construct()
+    {
+        global $DIC;
+        $this->db = $DIC->database();
+        
+        try {
+            parent::__construct();
+        } catch (ilPluginException $e) {
+            // Display Error-Message
+            $this->showRenamingNoticeAndExit();
+        }
+    }
+    
+    protected function afterUpdate()
+    {
+        if (PluginConfig::count() == 0) {
+            PluginConfig::importFromXML($this->getDirectory() . '/configuration/default_config.xml');
+        }
+    }
+    
+    protected function beforeUpdate()
+    {
+        try {
+            $this->checkPluginDirectory();
+        } catch (ilPluginException $e) {
+            $this->showRenamingNoticeAndExit();
+        }
+        return true;
+    }
+    
+    /**
+     * @return bool
+     */
+    protected function uninstallCustom()
+    {
+        $this->db->dropTable(PermissionGrant::TABLE_NAME, false);
+        $this->db->dropTable(PermissionGroupParticipant::TABLE_NAME, false);
+        $this->db->dropTable(PermissionGroup::TABLE_NAME, false);
+        $this->db->dropTable(ObjectSettings::TABLE_NAME, false);
+        $this->db->dropTable(EventAdditionsAR::TABLE_NAME, false);
+        $this->db->dropTable(PermissionTemplate::TABLE_NAME, false);
+        $this->db->dropTable(PublicationUsage::TABLE_NAME, false);
+        $this->db->dropTable(PluginConfig::TABLE_NAME, false);
+        $this->db->dropTable(Report::DB_TABLE, false);
+        $this->db->dropTable(WorkflowAR::TABLE_NAME, false);
+        $this->db->dropTable(WorkflowParameter::TABLE_NAME, false);
+        $this->db->dropTable(SeriesWorkflowParameter::TABLE_NAME, false);
+        $this->db->dropTable(MDFieldConfigEventAR::TABLE_NAME, false);
+        $this->db->dropTable(MDFieldConfigSeriesAR::TABLE_NAME, false);
+        $this->db->dropTable(UserSetting::TABLE_NAME, false);
+        $this->db->dropTable(AcceptedToU::TABLE_NAME, false);
+        $this->db->dropTable(DBCacheAR::TABLE_NAME, false);
+        
+        return true;
+    }
+    
+    public function getPluginName()
+    {
+        return self::PLUGIN_NAME;
+    }
+    
+    protected function showRenamingNoticeAndExit(string $additional_info = null) : void
+    {
+        echo $additional_info !== null ? $additional_info . nl2br("\n\n") : '';
+        echo $this->parseRenamingInfo();
+        exit();
+    }
+    
+    protected function parseRenamingInfo() : string
+    {
+        return nl2br(file_get_contents(__DIR__ . "/../doc/RENAMING_PLUGIN.md"));
+    }
+    
+    /**
+     * @throws Exception
+     */
+    protected function checkPluginDirectory() : void
+    {
+        // The Plugins has been renamed in 2022. Therefore, we have to check if
+        // the current Plugin-Name is equivalent to the directory.
+        $matches = [];
+        $plugin_name = (string) $this->getPluginName();
+        preg_match('/.*RepositoryObject\/(?<directory_name>.*)\/classes.*/m', __DIR__, $matches);
+        if (!isset($matches['directory_name']) || strlen($matches['directory_name'] < 8)) {
+            throw new ilPluginException('Could not determine directory name of plugin');
+        }
+        
+        $directory_name = $matches['directory_name'];
+        if ($plugin_name !== $directory_name) {
+            throw new ilPluginException(
+                "The Plugin directory `$directory_name` does not match the plugin name `$plugin_name`"
+            );
+        }
+    }
+    
+    public function allowCopy()
+    {
+        return true;
+    }
+    
+    public function updateLanguages($a_lang_keys = null)
+    {
+        parent::updateLanguages($a_lang_keys);
+        self::dataTableUI()->installLanguages(self::plugin());
+    }
+    
 }
--- plugin.php
+++ plugin.php
@@ -3,5 +3,5 @@ $id = 'xoct';
 $version = '4.0.1';
 $ilias_min_version = '6.0';
 $ilias_max_version = '7.999';
-$responsible = 'fluxlabs';
-$responsible_mail = 'support@fluxlabs.ch';
+$responsible = 'https://github.com/opencast-ilias';
+$responsible_mail = '-';
