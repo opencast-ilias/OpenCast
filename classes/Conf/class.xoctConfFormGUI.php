87518060e5cf26d4d130ca9bc36569873b40f0c4
okaufman <olivia.kaufmann@unibe.ch>
2022-05-17 11:59:18 +0200

    fix22: configure more than one input

--- classes/Conf/class.xoctConfFormGUI.php
+++ classes/Conf/class.xoctConfFormGUI.php
@@ -218,6 +218,7 @@ class xoctConfFormGUI extends ilPropertyFormGUI {
 
 		$te = new ilTextInputGUI($this->parent_gui->txt(PluginConfig::F_SCHEDULE_CHANNEL), PluginConfig::F_SCHEDULE_CHANNEL);
 		$te->setInfo($this->parent_gui->txt(PluginConfig::F_SCHEDULE_CHANNEL . '_info'));
+		$te->setMulti(true);
 		$this->addItem($te);
 
 		$cb = new ilCheckboxInputGUI($this->parent_gui->txt(PluginConfig::F_CREATE_SCHEDULED_ALLOWED), PluginConfig::F_CREATE_SCHEDULED_ALLOWED);
