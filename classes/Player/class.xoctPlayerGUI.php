0fe2a234ccc6f54676f7011c61499051774df41d
farbod <zamanifarbod2@gmail.com>
2023-07-25 13:09:42 +0200

    minor bug fix for style sheet set variable

--- classes/Player/class.xoctPlayerGUI.php
+++ classes/Player/class.xoctPlayerGUI.php
@@ -165,7 +165,7 @@ class xoctPlayerGUI extends xoctGUI
         if ($event->isLiveEvent()) {
             $tpl->setVariable(
                 "STYLE_SHEET_LOCATION",
-                ILIAS_HTTP_PATH . '/' . self::plugin()->getPluginObject()->getDirectory() . "/templates/default/player_w_chat.css"
+                ILIAS_HTTP_PATH . ltrim(self::plugin()->getPluginObject()->getDirectory(), '.') . "/templates/default/player_w_chat.css"
             );
             $ChatroomAR = ChatroomAR::findOrCreate($event->getIdentifier(), $this->objectSettings->getObjId());
             $public_name = self::dic()->user()->hasPublicProfile() ?
@@ -178,7 +178,7 @@ class xoctPlayerGUI extends xoctGUI
             // show chat history for past live events
             $tpl->setVariable(
                 "STYLE_SHEET_LOCATION",
-                ILIAS_HTTP_PATH . '/' . self::plugin()->getPluginObject()->getDirectory() . "/templates/default/player_w_chat.css"
+                ILIAS_HTTP_PATH . ltrim(self::plugin()->getPluginObject()->getDirectory(), '.') . "/templates/default/player_w_chat.css"
             );
             $ChatHistoryGUI = new ChatHistoryGUI($ChatroomAR->getId());
             $tpl->setVariable('CHAT', $ChatHistoryGUI->render(true));
