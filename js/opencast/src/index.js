37901770eec0449b89193239158371491cfdd7ca
FarbodZamani <53179227+ferishili@users.noreply.github.com>
2023-10-27 10:53:54 +0200

    Show/Hide API Password (#210)
    * fixes #206, hide/show password
    
    * fixes according to change requests
    
    * Moved code to first opencast js module
    
    * add authors
    
    ---------
    
    Co-authored-by: Fabian Schmid <fabian@sr.solutions>

---
+++ js/opencast/src/index.js
@@ -0,0 +1,7 @@
+import il from 'ilias';
+import $ from 'jquery';
+import PasswordToggle from './Form/PasswordToggle';
+
+il.Opencast = il.Opencast || {};
+il.Opencast.Form = il.Opencast.Form || {};
+il.Opencast.Form.passwordToggle = new PasswordToggle($);
