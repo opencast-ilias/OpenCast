03d9570e55c127e8a3b8c4844ea3c019b2209e99
Fabian Schmid <fabian@sr.solutions>
2023-11-17 16:52:21 +0100

    Updated Node-Modules and removed legacy paella player

--- node_modules/hls.js/src/types/component-api.ts
+++
@@ -1,8 +0,0 @@
-export interface ComponentAPI {
-  destroy(): void;
-}
-
-export interface NetworkComponentAPI extends ComponentAPI {
-  startLoad(startPosition: number): void;
-  stopLoad(): void;
-}
