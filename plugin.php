8fb2136359d36d6dbee8fcf2d022dc453f139c3b
Fabian Schmid <fabian@sr.solutions>
2023-11-24 16:19:40 +0100

    Requirements

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 $id = 'xoct';
 $version = '5.6.0';
 $version_check = '44ac530093a998b525b0a73ba536e64f03bbaff47446cf99e1a31d6a042a4549';
-$ilias_min_version = '6.0';
+$ilias_min_version = '7.0';
 $ilias_max_version = '7.999';
 $responsible = 'Universität Bern';
 $responsible_mail = 'david.graf@unibe.ch';
