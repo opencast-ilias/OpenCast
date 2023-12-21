219696498b4bca3785e9f21f3a09c7dfb5989d2d
Fabian Schmid <fabian@sr.solutions>
2023-12-21 09:29:17 +0100

    [FIX] missing isset

--- src/Model/Series/SeriesParser.php
+++ src/Model/Series/SeriesParser.php
@@ -25,7 +25,7 @@ class SeriesParser
         $series->setIdentifier($data->identifier);
         $series->setAccessPolicies($this->ACLParser->parseAPIResponse($data->acl ?? []));
         $series->setMetadata($data->metadata);
-        if (is_int($data->theme)) {
+        if (isset($data->theme) && is_int($data->theme)) {
             $series->setTheme($data->theme);
         }
         return $series;
