d6de0ca2180cea27589980ede3c13881781a6bb4
Fabian Schmid <fabian@sr.solutions>
2024-08-27 16:28:32 +0200

    [FIX] removed no longer used update check

--- src/Util/UpdateCheck.php
+++ src/Util/UpdateCheck.php
@@ -6,57 +6,17 @@ namespace srag\Plugins\Opencast\Util;
 
 class UpdateCheck
 {
-    private string $path_to_plugin_php = __DIR__ . '/../../plugin.php';
-    private \ilDBInterface $db;
-    private $last_update_version = '';
-    private string $version_check_string = '';
-    private $version_check_string_db = '';
+    private string $last_update_version = '';
 
     public function __construct(\ilDBInterface $db)
     {
-        $this->db = $db;
-        if (!is_readable($this->path_to_plugin_php)) {
-            throw new \Exception('Could not find plugin.php');
-        }
-        include $this->path_to_plugin_php; // read the infos from plugin.php
-        /** @noinspection IssetArgumentExistenceInspection */
-        if (isset($version_check)) {
-            $this->version_check_string = $version_check;
-        }
-
         // read old plugin version from db
-        $res = $this->db->queryF(
+        $res = $db->queryF(
             'SELECT last_update_version FROM il_plugin WHERE plugin_id = %s',
             ['text'],
             ['xoct']
         )->fetchObject();
         $this->last_update_version = $res->last_update_version ?? '0.0.0';
-
-        // version check string from db
-        try {
-            $res = $this->db->queryF(
-                'SELECT `value` FROM xoct_config WHERE `name` = %s',
-                ['text'],
-                ['version_check']
-            )->fetchObject();
-        } catch (\Throwable $t) {
-            $res = null; // unable to read from config, maybe a new installation of the plugin
-        }
-
-        $this->version_check_string_db = $res->value ?? '';
-    }
-
-    public function isUpdatePossible(): bool
-    {
-        // Older versions
-        if (version_compare($this->last_update_version, '4.0.2', '<=')) {
-            return true;
-        }
-        if ($this->version_check_string_db === '') {
-            return false;
-        }
-
-        return $this->version_check_string === $this->version_check_string_db;
     }
 
     public function isNewInstallation(): bool
