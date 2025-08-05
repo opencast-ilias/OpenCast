0c19cfe2eb56a0ad1590dffa41d5a1ffe01d2dd8
Fabian Schmid <fabian@sr.solutions>
2025-08-05 09:29:42 +0200

    [FIX] prepared remove all

--- classes/Invitations/class.xoctGrantPermissionGUI.php
+++ classes/Invitations/class.xoctGrantPermissionGUI.php
@@ -74,7 +74,8 @@ class xoctGrantPermissionGUI extends xoctGUI
             'LANGUAGE',
             json_encode([
                 'none_available' => $this->plugin->txt('invitations_none_available'),
-                'invite_all' => $this->plugin->txt('invitations_invite_all')
+                'invite_all' => $this->plugin->txt('invitations_invite_all'),
+                'remove_all' => $this->plugin->txt('invitations_remove_all'),
             ])
         );
         $this->main_tpl->setContent($temp->get());
@@ -236,6 +237,20 @@ class xoctGrantPermissionGUI extends xoctGUI
         ])->first();
         if ($obj instanceof PermissionGrant) {
             $obj->delete();
+        }
+    }
+
+    protected function deleteMultiple(): void
+    {
+        $ids = $this->http->request()->getParsedBody()['ids'] ?? [];
+        foreach ($ids as $id) {
+            $obj = PermissionGrant::where([
+                'event_identifier' => $this->event->getIdentifier(),
+                'user_id' => $id,
+            ])->first();
+            if ($obj instanceof PermissionGrant) {
+                $obj->delete();
+            }
         }
     }
 }
