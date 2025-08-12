0baf438de3bbad7edf55ab8e5072572b35bcf22a
Fabian Schmid <fabian@sr.solutions>
2025-08-12 09:32:29 +0200

    [FIX] several (UI) issues in Groups, Change-Owner and Invitation Screens
    commit 544262b383e51e53b278d279247bb8ce2bf1262f
    Author: Fabian Schmid <fabian@sr.solutions>
    Date:   Tue Aug 12 09:30:57 2025 +0200
    
        [FIX] changes due to review
    
    commit ebd79e1a0e8ca67e70a3d984ba33ef279a34836a
    Author: Fabian Schmid <fabian@sr.solutions>
    Date:   Tue Aug 5 09:29:58 2025 +0200
    
        [FIX] issue with addAll button if there are none left
    
    commit 0c19cfe2eb56a0ad1590dffa41d5a1ffe01d2dd8
    Author: Fabian Schmid <fabian@sr.solutions>
    Date:   Tue Aug 5 09:29:42 2025 +0200
    
        [FIX] prepared remove all
    
    commit 9294f9e8564714958ac2ee6e8d6a9992c63bd578
    Author: Fabian Schmid <fabian@sr.solutions>
    Date:   Tue Aug 5 09:21:39 2025 +0200
    
        [FIX] codestyle in JS
    
    commit de5b87a329688e4c5ff6aaabf741e10eaa94a107
    Author: Fabian Schmid <fabian@sr.solutions>
    Date:   Mon Aug 4 15:20:54 2025 +0200
    
        [FIX] badge has no color
    
    commit 5ec717684af5d226f61b69920d6061a60c25470a
    Author: Fabian Schmid <fabian@sr.solutions>
    Date:   Fri Jul 18 10:16:16 2025 +0200
    
        [FIX] Button issues
    
    commit 6b305ff91439bec9c71593c15617cdc6c7270dc9
    Author: Fabian Schmid <fabian@sr.solutions>
    Date:   Thu Jul 17 22:42:40 2025 +0200
    
        [FIX] CSS changes in groups and invitations for ILIAS 9
    
    commit d883f384fc3d35d9d787383af5b33713eb62cbd1
    Author: Fabian Schmid <fabian@sr.solutions>
    Date:   Fri Jul 4 14:34:54 2025 +0200
    
        [FIX] change owner and grant access fixed as well
    
    commit 52045272c9ebbf59325677fccdd71703eadaa428
    Author: Fabian Schmid <fabian@sr.solutions>
    Date:   Tue Jul 1 16:37:50 2025 +0200
    
        [FIX] "Groups" lacks of buttons to add/remove users #443
    
    (cherry picked from commit ab9ed91e7cd51449d453d16fbaffc3822be703f2)

--- templates/default/invitations.js
+++ templates/default/invitations.js
@@ -3,166 +3,191 @@
  *
  * @type {{init: Function, selected_id: number, data_url: string, load: Function, deleteGroup: Function, selectGroup: Function, deselectAll: Function, create: Function}}
  */
-var xoctInvitation = {
-	selected_id: 0,
-	data_url: '',
-	container: null,
-    filter_container: null,
-    filtering: false,
-	lng: [
-		delete_group = "Delete Group?",
-		no_title = "Please insert title",
-		none_available = "None available",
-		invite_all = "Invite All"
-	],
-	before_load: function () {
-	},
-	after_load: function () {
-	},
-	lngFromJson: function (lng_json) {
-		this.lng = JSON.parse(lng_json);
-	},
-	init: function (data_url, container_invited, container_available, before_load, after_load) {
-		if (typeof before_load != 'undefined') {
-			this.before_load = before_load;
-		}
-		if (typeof after_load != 'undefined') {
-			this.after_load = after_load;
-		}
+const xoctInvitation = {
+  selected_id: 0,
+  data_url: '',
+  container: null,
+  filter_container: null,
+  filtering: false,
+  lng: {
+    delete_group: 'Delete Group?',
+    no_title: 'Please insert title',
+    none_available: 'None available',
+    invite_all: 'Invite All',
+    remove_all: 'Remove All',
+  },
+  before_load() {
+  },
+  after_load() {
+  },
+  lngFromJson(lng_json) {
+    this.lng = JSON.parse(lng_json);
+  },
+  init(data_url, container_invited, container_available, before_load, after_load) {
+    if (typeof before_load !== 'undefined') {
+      this.before_load = before_load;
+    }
+    if (typeof after_load !== 'undefined') {
+      this.after_load = after_load;
+    }
 
-		this.data_url = data_url;
-		$(container_invited).html('<ul id="xoct_invitations" class="list-group"></ul>');
-		$(container_available).html(
-			'<button id="xoct_invite_all" class="btn btn-primary">'
-			+ this.lng.invite_all
-			+ '</button><ul id="xoct_available" class="list-group"></ul>'
-		);
-		this.container_invited = $('#xoct_invitations');
-		this.container_available = $('#xoct_available');
-        this.filter_container = $('#xoct_participant_filter');
-		this.load();
+    this.data_url = data_url;
+    $(container_invited).html(
+      `<button id="xoct_remove_all" class="btn btn-primary" style="display: none">${
+        this.lng.remove_all
+      }</button>`
+            + '<ul id="xoct_invitations" class="list-group"></ul>',
+    );
 
-		var self = this;
-		$(document).on('click', '.xoct_remove', function () {
-			self.removeInvitation($(this).parent().data('invitation-id'));
-		});
+    $(container_available).html(
+      `<button id="xoct_invite_all" class="btn btn-primary">${
+        this.lng.invite_all
+      }</button><ul id="xoct_available" class="list-group"></ul>`,
+    );
+    this.container_invited = $('#xoct_invitations');
+    this.container_available = $('#xoct_available');
+    this.filter_container = $('#xoct_participant_filter');
+    this.load();
 
-		$(document).on('click', '.xoct_invite', function () {
-			self.addInvitation($(this).parent().data('invitation-id'));
-		});
+    const self = this;
+    $(document).on('click', '.xoct_remove', function () {
+      self.removeInvitation($(this).parent().data('invitation-id'));
+    });
 
-		$.expr[':'].contains = function (a, i, m) {
-			return $(a).text().toUpperCase()
-			.indexOf(m[3].toUpperCase()) >= 0;
-		};
+    $(document).on('click', '.xoct_invite', function () {
+      self.addInvitation($(this).parent().data('invitation-id'));
+    });
 
-		this.filter_container.keyup(function () {
-				self.filter($(this).val());
-				if (self.filtering && !$('#xoct_filter').length) {
-						self.filter_container.after('<span class="input-group-btn"><button class="btn btn-default" id="xoct_filter" type="button"><span class="glyphicon glyphicon-remove"></span> </button></span>');
-				} else if (!self.filtering) {
-						$('#xoct_filter').remove();
-				}
-		});
+    $.expr[':'].contains = function (a, i, m) {
+      return $(a).text().toUpperCase()
+        .indexOf(m[3].toUpperCase()) >= 0;
+    };
 
+    this.filter_container.keyup(function () {
+      self.filter($(this).val());
+      if (self.filtering && !$('#xoct_filter').length) {
+        self.filter_container.after('<span class="input-group-btn"><button class="btn btn-default" id="xoct_filter" type="button"><span class="glyphicon glyphicon-remove"></span> </button></span>');
+      } else if (!self.filtering) {
+        $('#xoct_filter').remove();
+      }
+    });
 
-		$(document).on('click', '#xoct_filter', function () {
-				self.filter_container.val('');
-				self.filter('');
-				$(this).remove();
-		});
+    $(document).on('click', '#xoct_filter', function () {
+      self.filter_container.val('');
+      self.filter('');
+      $(this).remove();
+    });
 
-		$(document).on('click', '#xoct_invite_all', function() {
-			self.inviteAll();
-		})
+    $(document).on('click', '#xoct_invite_all', () => {
+      self.inviteAll();
+    });
 
-	},
-	clear: function () {
-		this.container_invited.empty();
-		this.container_available.empty();
-	},
+    $(document).on('click', '#xoct_remove_all', () => {
+      self.removeAll();
+    });
+  },
+  clear() {
+    this.container_invited.empty();
+    this.container_available.empty();
+  },
 
-	load: function () {
-		var self = this;
-		this.before_load();
-		var url = this.data_url;
-		$.ajax({url: url, type: "GET", data: {"cmd": "getAll"}}).done(function (data) {
-			self.clear();
-			for (var i in data.available) {
-				self.container_available.append('<li class="list-group-item xoct_participant_available" data-invitation-id="' + data.available[i].id + '">'
-					+ '<div style="margin-right:30px;">'
-					+ data.available[i].name
-					+ '</div>'
-					+ '<button class="btn btn-primary xoct_invite pull-right"><span class="glyphicon glyphicon-plus"></span></button>'
-					+ '</li>');
-			}
+  load() {
+    const self = this;
+    this.before_load();
+    const url = this.data_url;
+    $.ajax({ url, type: 'GET', data: { cmd: 'getAll' } }).done((data) => {
+      self.clear();
+      for (var i in data.available) {
+        self.container_available.append(`<li class="list-group-item xoct_participant_available" data-invitation-id="${data.available[i].id}">`
+                    + `<div style="margin-right:30px;">${
+                      data.available[i].name
+                    }</div>`
+                    + '<button class="btn btn-primary xoct_invite pull-right"><span class="glyphicon glyphicon-plus"></span></button>'
+                    + '</li>');
+      }
 
-			for (var i in data.invited) {
-				self.container_invited.append('<li class="list-group-item" data-invitation-id="' + data.invited[i].id + '">'
-					+ '<div style="margin-right:30px;">'
-					+ data.invited[i].name
-					+ '</div>'
-					+ '<button class="btn btn-default xoct_remove pull-right"><span class="glyphicon glyphicon-minus"></span></button>'
-					+ '</li>');
-			}
+      for (var i in data.invited) {
+        self.container_invited.append(`<li class="list-group-item" data-invitation-id="${data.invited[i].id}">`
+                    + `<div style="margin-right:30px;">${
+                      data.invited[i].name
+                    }</div>`
+                    + '<button class="btn btn-default xoct_remove pull-right"><span class="glyphicon glyphicon-minus"></span></button>'
+                    + '</li>');
+      }
 
-			if (!data.available || data.available.length == 0) {
-				self.container_available.html('<li class="list-group-item">' + self.lng.none_available + '</li>');
-			}
+      if (!data.available || data.available.length == 0) {
+        self.container_available.html(`<li class="list-group-item">${self.lng.none_available}</li>`);
+      }
 
-			if (!data.invited || data.invited.length == 0) {
-				self.container_invited.html('<li class="list-group-item">' + self.lng.none_available + '</li>');
-			}
+      if (!data.invited || data.invited.length == 0) {
+        self.container_invited.html(`<li class="list-group-item">${self.lng.none_available}</li>`);
+      }
 
-			self.after_load();
-		});
-	},
+      self.after_load();
+    });
+  },
 
-	addInvitation:function (id) {
-		var url = this.data_url;
-		var self = this;
-		this.before_load();
+  addInvitation(id) {
+    const url = this.data_url;
+    const self = this;
+    this.before_load();
 
-		$.ajax({url: url + "&cmd=create", type: "POST", data: {"id": id}}).done(function (data) {
-			self.load();
-			self.after_load();
-		});
-	},
+    $.ajax({ url: `${url}&cmd=create`, type: 'POST', data: { id } }).done((data) => {
+      self.load();
+      self.after_load();
+    });
+  },
 
-	inviteAll: function() {
-		var ids = [];
-		$('#xoct_available li:visible').each(function(i) {
-			ids.push($(this).attr('data-invitation-id'));
-		});
+  inviteAll() {
+    const ids = [];
+    $('#xoct_available li:visible').each(function (i) {
+      ids.push($(this).attr('data-invitation-id'));
+    });
 
-		var url = this.data_url;
-		var self = this;
-		this.before_load();
+    const url = this.data_url;
+    const self = this;
+    this.before_load();
 
-		$.ajax({url: url + "&cmd=createMultiple", type: "POST", data: {"ids": ids}}).done(function (data) {
-			self.load();
-			self.after_load();
-		});
-	},
+    $.ajax({ url: `${url}&cmd=createMultiple`, type: 'POST', data: { ids } }).done((data) => {
+      self.load();
+      self.after_load();
+    });
+  },
 
-	removeInvitation:function (id) {
-		var url = this.data_url;
-		var self = this;
-		this.before_load();
-		$.ajax({url: url + "&cmd=delete", type: "POST", data: {"id": id}}).done(function (data) {
-			$('[data-invitation-id="' + id + '"]').remove();
-			self.load();
-			self.after_load();
-		});
-	},
+  removeInvitation(id) {
+    const url = this.data_url;
+    const self = this;
+    this.before_load();
+    $.ajax({ url: `${url}&cmd=delete`, type: 'POST', data: { id } }).done((data) => {
+      $(`[data-invitation-id="${id}"]`).remove();
+      self.load();
+      self.after_load();
+    });
+  },
 
-    /**
+  removeAll() {
+    const ids = [];
+    $('#xoct_invitations li:visible').each(function (i) {
+      ids.push($(this).attr('data-invitation-id'));
+    });
+
+    const url = this.data_url;
+    const self = this;
+    this.before_load();
+
+    $.ajax({ url: `${url}&cmd=deleteMultiple`, type: 'POST', data: { ids } }).done((data) => {
+      self.load();
+      self.after_load();
+    });
+  },
+
+  /**
      *
      * @param string
      */
-    filter: function (string) {
-        this.filtering = (string != '');
-        $('.xoct_participant_available:not(:contains("' + string + '"))').hide();
-        $('.xoct_participant_available:contains("' + string + '")').show();
-    }
+  filter(string) {
+    this.filtering = (string != '');
+    $(`.xoct_participant_available:not(:contains("${string}"))`).hide();
+    $(`.xoct_participant_available:contains("${string}")`).show();
+  },
 };
