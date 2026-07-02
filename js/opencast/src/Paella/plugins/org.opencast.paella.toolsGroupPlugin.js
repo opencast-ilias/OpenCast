41bffff6485710c051c264727bae55320e3765a6
FarbodZamani <53179227+ferishili@users.noreply.github.com>
2026-07-02 16:10:50 +0200

    paella 8 upgrade

--- js/opencast/src/Paella/plugins/org.opencast.paella.toolsGroupPlugin.js
+++
@@ -1,9 +0,0 @@
-import { ButtonGroupPlugin } from 'paella-core';
-
-import MenuIcon from '../icons/settings-icon.svg';
-
-export default class ToolsGroupPlugin extends ButtonGroupPlugin {
-    async load() {
-        this.icon = this.player.getCustomPluginIcon(this.name, 'buttonIcon') || MenuIcon;
-    }
-}
